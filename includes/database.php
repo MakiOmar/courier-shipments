1dbb7d5364f39bb943cd3426942be4488f09aed0
makiomar <mo7amed.maki@gmail.com>
2024-12-15 17:57:59 +0200

    Database shipment tracking table

--- includes/database.php
+++ includes/database.php
@@ -19,28 +19,22 @@ function create_shipment_tracking_table() {
 
 	// Get the character set and collation.
 	$charset_collate = $wpdb->get_charset_collate();
+    // SQL to create the table.
+    $sql = "CREATE TABLE IF NOT EXISTS {$table_name} (
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            shipment_id BIGINT UNSIGNED NOT NULL,
+            employee_id BIGINT UNSIGNED NOT NULL,
+            status ENUM('collected', 'packaged', 'processing', 'shipped', 'delivered') NOT NULL,
+            description TEXT,
+            created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (shipment_id) REFERENCES {$wpdb->prefix}jet_cct_shipment(_ID) ON DELETE CASCADE ON UPDATE CASCADE,
+            FOREIGN KEY (employee_id) REFERENCES {$wpdb->prefix}users(ID) ON DELETE CASCADE ON UPDATE CASCADE
+        ) $charset_collate;
+    ";
+    // Include the WordPress upgrade library.
+    require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
-	// Check if the table exists.
-	if ( $wpdb->get_var( "SHOW TABLES LIKE '{$table_name}'" ) !== $table_name ) {
-		// SQL to create the table.
-		$sql = "
-            CREATE TABLE {$table_name} (
-                id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-                shipment_id BIGINT UNSIGNED NOT NULL,
-                employee_id BIGINT UNSIGNED NOT NULL,
-                status ENUM('collected', 'packaged', 'processing', 'shipped', 'delivered') NOT NULL,
-                description TEXT,
-                created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
-                FOREIGN KEY (shipment_id) REFERENCES {$wpdb->prefix}jet_cct_shipment(_ID) ON DELETE CASCADE ON UPDATE CASCADE,
-                FOREIGN KEY (employee_id) REFERENCES {$wpdb->prefix}users(ID) ON DELETE CASCADE ON UPDATE CASCADE
-            ) $charset_collate;
-        ";
-
-		// Include the WordPress upgrade library.
-		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-
-		// Execute the SQL to create the table.
-		dbDelta( $sql );
-	}
+    // Execute the SQL to create the table.
+    dbDelta( $sql );
 }
 
