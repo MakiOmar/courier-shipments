e8d94f9ce93982b49701ff9c329237953c306357
makiomar <mo7amed.maki@gmail.com>
2025-01-08 13:47:04 +0200

    Waybill print design

--- includes/hooks.php
+++ includes/hooks.php
@@ -21,50 +21,124 @@ add_action(
 	'admin_footer',
 	function ( $hook ) {
 
-		if ( isset( $_GET['page'] ) && ! empty( $_GET['page'] && 'jet-cct-shipment' === $_GET['page'] ) ) {
+		if ( isset( $_GET['page'] ) && ! empty( $_GET['page'] && 'jet-cct-shipments' === $_GET['page'] ) ) {
 			?>
 			<script>
-				function printQr( data ) {
-					// Generate print content
-					var printContent = '<div style="font-family: Arial; padding: 20px;">';
-					data.forEach(function(shipment) {
+				function printQr(data) {
+					let printContent = '';
+					data.forEach(function (shipment) {
 						let trackingNumber = shipment.tracking_number;
-						printContent += '<div style="page-break-after: always;display:flex;flex-direction:column; align-items: center; justify-content:center; direction: rtl; margin: 0 auto;">' +
-							'<img src="<?php echo COURSH_LOGO; ?>" alt="Logo" style="max-width: 150px; margin-bottom: 10px; display: inline-block;">' +
-							'<table style="width: auto; margin: 0 auto; border-collapse: collapse;font-size:15px">' +
-								'<tr><td style="text-align: right;padding: 10px 0;min-width:75px"><strong>' + '<?php esc_html_e( 'Description', 'coursh' ); ?>' + ': </strong></td><td style="text-align: right;">' + shipment.contentdescription + '</td></tr>' +
-							'</table>' +
-							'<div style="margin-top: 10px;">' +
-								'<img src="https://api.qrserver.com/v1/create-qr-code/?data=' + trackingNumber + '&size=80x80" alt="QR Code for shipment ID" style="display: block; max-width: 150px; margin: 0 auto;">' +
-							'</div>';
+						printContent += `
+							<div class="suc-waybill-container" style="border: 2px solid #000; font-size: 14px; padding: 10px; direction: rtl; page-break-after: always;">
+								<!-- Header -->
+								<div class="suc-waybill-header" style="background-color: #30388d; color: #fff; text-align: center; padding: 10px;">
+									<h5 style="margin: 0;">بوليصة الشحن WayBill</h5>
+								</div>
+								<!-- Subheader with Logo and Barcode -->
+								<div class="suc-waybill-subheader" style="background-color: #30388d;display: flex; align-items: center; justify-content: space-between; padding: 10px; border-bottom: 2px solid #000;">
+									<div style="text-align: center;">
+										<img src="https://courier.lightway-eg.com/wp-content/uploads/2025/01/waybill-logo.png" alt="Logo" style="max-width: 100px;">
+									</div>
+									<div style="width: 75%;display:flex;justify-content:center">
+										<div style="text-align: center; background-color: #fff; border-radius: 5px; color: #000;width: 75%;">
+											<img src="https://api.qrserver.com/v1/create-qr-code/?data=${trackingNumber}&size=80x80" alt="QR Code for shipment ID" style="max-height: 80px;">
+											<p style="margin: 5px 0;">${trackingNumber}</p>
+										</div>
+									</div>
+									<div style="text-align: center;">
+										<img src="https://courier.lightway-eg.com/wp-content/uploads/2025/01/waybill-logo.png" alt="Logo" style="max-width: 100px;">
+									</div>
+								</div>
+								<!-- Sender and Receiver Info -->
+								<table style="width: 100%; border-collapse: collapse; margin-top: 10px;">
+									<thead>
+										<tr>
+											<th style="background-color: #30388d; color: #fff; font-size: 22px; text-align: center; padding: 10px; border: 1px solid #000;">Sender Info</th>
+											<th style="background-color: #30388d; color: #fff; font-size: 22px; text-align: center; padding: 10px; border: 1px solid #000;">Receiver Info</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>اسم المرسل:</strong> شركة الجوهرة</td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>اسم المستلم:</strong> خليفة المهري</td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>عنوان المرسل:</strong></td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>عنوان المستلم:</strong> شارع الإعلامي</td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>الدولة:</strong></td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>الدولة:</strong> قطر</td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>المدينة:</strong></td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>المدينة:</strong> الدوحة</td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>الهاتف:</strong></td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>الهاتف:</strong> +97455515580</td>
+										</tr>
+									</tbody>
+								</table>
+								<!-- Shipment and Payment Info -->
+								<table style="width: 100%; border-collapse: collapse; margin-top: 10px;">
+									<thead>
+										<tr>
+											<th style="background-color: #30388d; color: #fff; font-size: 22px; text-align: center; padding: 10px; border: 1px solid #000;">Shipment Info</th>
+											<th style="background-color: #30388d; color: #fff; font-size: 22px; text-align: center; padding: 10px; border: 1px solid #000;">Payment Info</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>تاريخ الشحنة:</strong> 2024/02/14</td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>Bill To:</strong></td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>الوزن الفعلي:</strong> KG 0.7</td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>التكلفة:</strong></td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>الوزن الإجمالي:</strong> KG 0.7</td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>Fees of COD:</strong></td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>عدد القطع:</strong> 1</td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>Other Charges:</strong></td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>المحتويات:</strong></td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>Insurance Charges:</strong></td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"></td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>Customs Charges:</strong></td>
+										</tr>
+										<tr>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"></td>
+											<td style="border: 1px solid #000; text-align: right; padding: 10px; font-size: 17px;"><strong>Total:</strong></td>
+										</tr>
+									</tbody>
+								</table>
+								<!-- Footer -->
+								<div style="margin-top: 10px; text-align: center;padding-bottom:50px">
+									<p style="background-color: #30388d;color:#fff;padding:10px;margin: 10px 0;">الشروط والأحكام Terms & Conditions</p>
+									<div style="display: flex; justify-content: space-between; margin-top: 10px;">
+										<span>توقيع العميل</span>
+										<span>التوقيع</span>
+									</div>
+								</div>
+							</div>`;
 					});
 
-					printContent += '</div>';
-
-					// Open print window
-					var printWindow = window.open('', '_blank');
+					let printWindow = window.open('', '_blank');
 					printWindow.document.write(`
 						<html>
 							<head>
-								<title>` + `<?php esc_html__( 'Print', 'coursh' ); ?>` + `</title>
 								<style>
 									@media print {
-										/* Set custom page size to 10x15 cm */
-										@page {
-											size: 10cm 15cm; /* Width x Height in cm */
-											margin: 2mm; /* Adjust margin as needed */
-										}
-
-										/* Ensure each shipment starts on a new page */
-										div { page-break-after: always; }
-
-										/* Ensure images display correctly */
-										img { display: block !important; max-width: 100% !important; }
-
-										/* Adjust body styling for compact size */
 										body {
-											font-family: Arial, sans-serif;
-											font-size: 12px; /* Adjust for smaller page */
+											-webkit-print-color-adjust: exact;
+											print-color-adjust: exact;
 										}
 									}
 								</style>
@@ -74,13 +148,12 @@ add_action(
 					`);
 
 					printWindow.document.close();
-
-					// Wait for images to load before printing
 					printWindow.onload = function () {
 						printWindow.print();
 					};
-
 				}
+
+
 				jQuery(document).ready(function ($) {
 					// Check if the bulk action selector exists
 					const bulkActionSelector = $('#bulk-action-selector-top');
