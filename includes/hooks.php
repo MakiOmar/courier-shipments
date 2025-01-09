7caac9c8a1ed95842da2b14034b2d9d7b0d01eb8
makiomar <mo7amed.maki@gmail.com>
2025-01-09 13:09:43 +0200

    Remove duplicate bootstrap

--- includes/hooks.php
+++ includes/hooks.php
@@ -12,7 +12,6 @@ add_action(
 	'wp_enqueue_scripts',
 	function () {
 		wp_enqueue_script( 'html5-qrcode', 'https://unpkg.com/html5-qrcode', array(), null, true );
-		wp_enqueue_style( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css' );
 	}
 );
 
